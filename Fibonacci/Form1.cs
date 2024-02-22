a2a28ed6da540f085d30f1a5b24745f9fb357389
mlevente0703 <mlevente0703@gmail.com>
2024-02-22 15:03:49 +0100

    Add project files.

---
+++ Fibonacci/Form1.cs
@@ -0,0 +1,35 @@
+namespace Fibonacci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Sor> sorok = new List<Sor>();
+            for (int i = 0; i < 20; i++)
+            {
+                Sor újsor = new Sor();
+                újsor.Sorszám = i;
+                újsor.Érték = Fibonacci(i);
+                sorok.Add(újsor);
+                Button button = new Button();
+                Controls.Add(button);
+                button.Text = Fibonacci(i).ToString();
+                button.Top = i * 20;
+                button.Left = 250;
+            }
+            dataGridView1.DataSource = sorok;
+        }
+
+        int Fibonacci(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
